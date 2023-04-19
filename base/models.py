d590e477981689637ef44251689915c5dd20074e
sakar-n <nirsakar@gmail.com>
2023-04-19 14:51:15 +0545

    final commit

--- base/models.py
+++ base/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Task(models.Model):
+class User(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     title = models.CharField(max_length=200)
